0789b89511724f8a874dbc2f07ab9ae04de86ed7
alvianzf <alvianzf@gmail.com>
2024-08-21 10:12:30 +0700

    add footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Layout({ children }) {
-  return <div>{children}</div>;
+  return <div>{children}
+  <footer style={{ left: "0", position: "absolute", bottom: '0', width: "100%", padding: "1em", textAlign: "center", fontSize: "0.5em"}}>
+<p>Alvian Zachry Faturrahman</p>
+  </footer>
+  </div>;
 }
 
 export default Layout;
