11735ee9f0f3be7762b66e8bf1ec7186043b3091
alvianzf <alvianzf@gmail.com>
2024-08-27 11:40:21 +0700

    refactor: change xml formatting method to a library for a cleaner look

---
+++ src/utils/xml/xml_formatter copy.js
@@ -0,0 +1,69 @@
+class XMLFormatter {
+  constructor(data) {
+    this.data = data;
+  }
+
+  getFormattedXML() {
+    return this.headerFormatter() + this.bodyFormatter() + this.constructor.XML_FOOTER;
+  }
+
+  headerFormatter() {
+    let content = "";
+
+    for (let i = 0; i < this.constructor.HEADER_DATA.length; i++) {
+      if (this.data[this.constructor.HEADER_DATA[i]]) {
+        content += `<${this.constructor.HEADER_DATA[i]}>${this.data[this.constructor.HEADER_DATA[i]]}</${this.constructor.HEADER_DATA[i]}>
+      `;
+      }
+    }
+
+    const header = this.constructor.XML_HEADER + content + this.constructor.XML_BREAK;
+    return header;
+  }
+
+  bodyFormatter() {
+    const length = this.data[this.data.headers[0]].length;
+    let content = "";
+
+    for (let i = 0; i < length; i++) {
+      content += `<KMS>
+`;
+      for (let j = 0; j < this.data.headers.length; j++) {
+        if (this.data[this.data.headers[j]][i]) {
+          content += `<${this.data.headers[j]}>${this.data[this.data.headers[j]][i]}</${this.data.headers[j]}>
+`;
+        }
+      }
+      content += `</KMS>`;
+    }
+
+    return content;
+  }
+}
+
+// XMLFormatter.XML_HEADER = `<?xml version="1.0" encoding="utf-8"?>
+// <DOCUMENT xmlns="cocokms.xsd">
+// <COCOKMS>
+// <HEADER>
+// `;
+// XMLFormatter.XML_BREAK = `</HEADER>
+// <DETIL>
+// `;
+// XMLFormatter.XML_FOOTER = `
+// </DETIL>
+// </COCOKMS>
+// </DOCUMENT>
+// `;
+
+// XMLFormatter.HEADER_DATA = [
+//   "KD_DOK",
+//   "KD_TPS",
+//   "NM_ANGKUT",
+//   "NO_VOY_FLIGHT",
+//   "CALL_SIGN",
+//   "TGL_TIBA",
+//   "KD_GUDANG",
+//   "REF_NUMBER",
+// ];
+
+export default XMLFormatter;
--- src/utils/xml/xml_formatter.js
+++ src/utils/xml/xml_formatter.js
@@ -1,59 +1,82 @@
+import { create } from "xmlbuilder2";
+
+/**
+ * XMLFormatter is a class that formats data into XML according to the specified schema.
+ * The class allows for the creation of XML headers and content, based on the provided data.
+ *
+ * Usage:
+ * const formatter = new XMLFormatter(data);
+ * formatter.headerFormatter();
+ * formatter.contentFormatter();
+ * const xmlString = formatter.formatXML();
+ *
+ * - `data`: An object containing the data to be formatted into XML.
+ * - `HEADER_DATA`: A static array that defines the headers to be included in the XML.
+ *
+ * Methods:
+ * - `headerFormatter()`: Formats and adds the header section of the XML.
+ * - `contentFormatter()`: Formats and adds the content section (DETIL) of the XML.
+ * - `getFormattedXML()`: Finalizes and returns the formatted XML string.
+ * 
+ * Author: Alvian Zachry F, M.Si
+ * Github: https://github.com/alvianzf
+ */
+
 class XMLFormatter {
   constructor(data) {
     this.data = data;
-  }
-
-  getFormattedXML() {
-    return this.headerFormatter() + this.bodyFormatter() + this.constructor.XML_FOOTER;
+    this.XML = null;
   }
 
   headerFormatter() {
-    let content = "";
+    const doc = create({ version: "1.0", encoding: "utf-8" })
+      .ele("DOCUMENT", { xmlns: "cocokms.xsd" })
+      .ele("COCOKMS")
+      .ele("HEADER");
 
-    for (let i = 0; i < this.constructor.HEADER_DATA.length; i++) {
-      if (this.data[this.constructor.HEADER_DATA[i]]) {
-        content += `<${this.constructor.HEADER_DATA[i]}>${this.data[this.constructor.HEADER_DATA[i]]}</${this.constructor.HEADER_DATA[i]}>
-      `;
+    for (let i = 0; i < XMLFormatter.HEADER_DATA.length; i++) {
+      const headerKey = XMLFormatter.HEADER_DATA[i];
+      if (this.data.hasOwnProperty(headerKey)) {
+        doc.ele(headerKey).txt(this.data[headerKey]);
       }
     }
 
-    const header = this.constructor.XML_HEADER + content + this.constructor.XML_BREAK;
-    return header;
+    doc.up();
+    this.XML = doc;
   }
 
-  bodyFormatter() {
-    const length = this.data[this.data.headers[0]].length;
-    let content = "";
+  contentFormatter() {
+    if (!this.XML) {
+      throw new Error("Header must be formatted first.");
+    }
 
+    const detailElement = this.XML.ele("DETIL");
+    const length = this.data[this.data.headers[0]].length;
     for (let i = 0; i < length; i++) {
-      content += `<KMS>
-`;
+      const kmsElement = detailElement.ele("KMS");
+
       for (let j = 0; j < this.data.headers.length; j++) {
-        if (this.data[this.data.headers[j]][i]) {
-          content += `<${this.data.headers[j]}>${this.data[this.data.headers[j]][i]}</${this.data.headers[j]}>
-`;
+        const header = this.data.headers[j];
+        const value = this.data[header][i];
+        
+        if (!XMLFormatter.HEADER_DATA.includes(header)) {
+          kmsElement.ele(header).txt(value);
         }
       }
-      content += `</KMS>`;
+
+      kmsElement.up();
     }
 
-    return content;
+    detailElement.up();
+  }
+
+  getFormattedXML() {
+    if (!this.XML) {
+      throw new Error("Header must be formatted first.");
+    }
+    return this.XML.end({ prettyPrint: true });
   }
 }
-
-XMLFormatter.XML_HEADER = `<?xml version="1.0" encoding="utf-8"?>
-<DOCUMENT xmlns="cocokms.xsd">
-<COCOKMS>
-<HEADER>
-`;
-XMLFormatter.XML_BREAK = `</HEADER>
-<DETIL>
-`;
-XMLFormatter.XML_FOOTER = `
-</DETIL>
-</COCOKMS>
-</DOCUMENT>
-`;
 
 XMLFormatter.HEADER_DATA = [
   "KD_DOK",
