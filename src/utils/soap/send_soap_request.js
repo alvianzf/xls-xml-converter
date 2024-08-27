eb1ab37d52f7a22d0d4d09a63db8af84596534a6
alvianzf <alvianzf@gmail.com>
2024-08-27 12:57:22 +0700

    fix: change soap request funtion

--- src/utils/soap/send_soap_request.js
+++ src/utils/soap/send_soap_request.js
@@ -1,31 +1,20 @@
 import axios from "axios";
 
 const SERVICE_NAME = import.meta.env.VITE_SERVICE_NAME;
-// const ENDPOINT = import.meta.env.VITE_ENDPOINT;
-const ENDPOINT = "../proxy/proxy";
+const ENDPOINT = "/api/proxy";
 const USERNAME = import.meta.env.VITE_SOAP_USERNAME;
 const PASSWORD = import.meta.env.VITE_SOAP_PASSWORD;
 
-const proxyUrl = "https://cors-anwhere.herokuapp.com/";
-
 const soapRequest = async (xmlData) => {
-  const envelope = `
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:coc="cocokms.xsd">
-    <soapenv:Header/>
-    <soapenv:Body>
-        <coc:${SERVICE_NAME}>
-            <coc:username>${USERNAME}</coc:username>
-            <coc:password>${PASSWORD}</coc:password>
-            <coc:fstream><![CDATA[${xmlData}]]></coc:fstream>
-        </coc:${SERVICE_NAME}>
-    </soapenv:Body>
-</soapenv:Envelope>`;
-
   try {
-    const response = await axios.post(ENDPOINT, envelope, {
+    const response = await axios.post(ENDPOINT, {
+      xmlData,
+      serviceName: SERVICE_NAME,
+      username: USERNAME,
+      password: PASSWORD,
+    }, {
       headers: {
-        "Content-Type": "text/xml",
-        Accept: "application/xml, text/xml",
+        "Content-Type": "application/json",
       },
     });
     return response.data;
